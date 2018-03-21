4b2e9f112b8e3d0a888f20947a3b920d8981af57
lsilva <lsilva@webmotors.com.br>
2018-03-21 11:17:36 -0300

    initial commit

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Top from './Header/index';
+import Routes from './Routes/index';
+import Footer from './Footer/index';
+
+const App = () => {
+  return (
+  <div>
+    <Top />
+    <Routes />
+    <Footer />
+  </div>
+  );
+};
+
+export default App;
